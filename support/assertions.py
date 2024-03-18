27a8aac0f7367ec5fc42f272ff867034fa5b9d46
IskhakovDanis <149081026+IskhakovDanis@users.noreply.github.com>
2024-03-18 19:07:01 +0000

    Deploying to gh-pages from @ IskhakovDanis/test-isolate-project@782522f609d0c7fc082d97e8f7c6d94c51631435 🚀

--- support/assertions.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Assertions:
-
-
-    @staticmethod
-    def assert_equal(actual, expected):
-        assert actual == expected, f'expected is {expected} but  actual is {actual}'
--- tests/test_stores.py
+++
@@ -1,4 +0,0 @@
-from env_config import ExpectedData
-
-def test_get_all_stores(stores_route):
-    stores_route.get_stores(expected_data=ExpectedData.STORES)
